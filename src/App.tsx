89f6ee3452bd8baab166237c22d29295b6aec131
Daniel Newton <kdaniel.newton@gmail.com>
2023-03-03 21:41:27 -0600

    Remove default content and add tailwindcss

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,4 @@
 import { useEffect, useState } from 'react';
-import reactLogo from './assets/react.svg';
-import './App.css';
 import useSupabaseDb from './hooks/useSupabaseDb';
 import { formatDateYmd } from '@/utils/date';
 
@@ -24,23 +22,9 @@ function App() {
   }, []);
 
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <h2>Fact of the Day</h2>
-        {fact && <p>{fact}</p>}
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <div>
+      <h2>Fact of the Day</h2>
+      {fact && <p>{fact}</p>}
     </div>
   );
 }
